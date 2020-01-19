c895e0292da1b7e3860107c95bec77cfbb97ad63
AndersonAndrad <Anderson_Andrade_@outlook.com>
2020-01-19 15:55:56 -0300

    [Update] - Adding global styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 import Routes from './routes';
 
+// styles
+import GlobalStyles from './pages/styles/global';
+
 function App() {
   return (
+    <>
     <Routes></Routes>
+    <GlobalStyles></GlobalStyles>
+    </>
   );
 }
 
