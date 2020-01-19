893a4cd03be520d635dbce7e31a41e8023219871
AndersonAndrad <Anderson_Andrade_@outlook.com>
2020-01-19 15:58:19 -0300

    [Update] - Changing default text color

--- src/pages/Main/style.js
+++ src/pages/Main/style.js
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const Title = styled.h1 `
    font-size: 24px;
-   color: #F14F16;
+   color: #fff;
    font-family: Arial, Helvetica, sans-serif;
 `;
