94599d51b7454b930243f59db3fade59bf06f29c
Anderson Andrade <Anderson_Andrade_@outlook.com>
2020-01-21 13:04:43 -0300

    [Update] - Create components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-import { Title } from './style';
+// icons
+import { FaGithubAlt, FaPlus } from 'react-icons/fa'
+
+import { Container, Form, SubmitButton } from './style';
 
 export default function Main() {
-  return <Title>Hello word</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt></FaGithubAlt>
+        Repositories
+      </h1>
+
+      <Form onSubmit = {() => { }}>
+        <input
+        type="text"
+        placeholder='Add repository'
+        />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14}></FaPlus>
+        </SubmitButton>
+      </Form>
+
+    </Container>
+  );
 }
