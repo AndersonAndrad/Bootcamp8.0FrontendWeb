0925034b3492cdfca372a530edad368ccd05c5e1
AndersonAndrad <Anderson_Andrade_@outlook.com>
2020-01-22 22:18:38 -0300

    [Update] - Styled componnet list

--- src/pages/Main/style.js
+++ src/pages/Main/style.js
@@ -66,5 +66,30 @@ export const SubmitButton = styled.button.attrs(props =>({
       animation: ${rotate} 2s linear infinite;
     }
   `}
+`;
 
-`
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 2px solid #eee;
+    }
+
+    a {
+    color: #F14F16;
+    text-decoration: none;
+    }
+  }
+
+
+
+
+`;
